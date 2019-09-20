ce2ca2b8a44a67fd460041ddfd823bbf0c0395b4
mnegal <maxim.negaliuc@endava.com>
2019-09-20 14:51:24 +0300

    logging middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,19 @@
-import { applyMiddleware, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
-import { verifyAuth } from "./actions/";
-import rootReducer from "./reducers";
+import { verifyAuth } from './actions/';
+import rootReducer from './reducers';
+
+const logMiddleware = store => dispatch => action => {
+  console.log(action.type, store.getState());
+  return dispatch(action);
+};
 
 export default function configureStore(persistedState) {
   const store = createStore(
     rootReducer,
     persistedState,
-    applyMiddleware(thunkMiddleware)
+    applyMiddleware(thunkMiddleware, logMiddleware)
   );
   store.dispatch(verifyAuth());
   return store;
