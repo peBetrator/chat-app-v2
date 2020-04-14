ad3507db9a2ea91236bec15f38be4958f37020a4
mnegal <fivalit@gmail.com>
2020-04-14 15:36:13 +0300

    bootstraped modal

--- src/features/components/common/modal.js
+++ src/features/components/common/modal.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 import './index.css';
 
-import SVGIcon from './svg';
-
 const Modal = props => {
+  const { title } = props;
   const { handleClose } = props.children.props;
 
   return createPortal(
-    <div className="custom__modal">
-      <section className="modal__main">
-        <div className="modal__close" onClick={handleClose}>
-          <SVGIcon name="close" width={10} />
+    <div className="modal d-block bg-modal">
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          {title && (
+            <div className="modal-header">
+              <h5 className="modal-title">{title}</h5>
+              <button type="button" className="close" onClick={handleClose}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          )}
+          <div className="modal-body">{props.children}</div>
         </div>
-        {props.children}
-      </section>
+      </div>
     </div>,
     document.body
   );
